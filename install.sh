7abbb03c7b127e3ddc22e64f93a78dc6343c0706
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-07 16:52:08 -0300

    Fix previous commit

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ echo "- Installing programs..."
    echo "- Cleanning install, saving settings..."
    $maysudo rm /usr/lib/floflis/layers/grass/install.sh
    $maysudo sed -i 's/grass/base/g' /usr/lib/floflis/config && $maysudo sed -i 's/soil/grass/g' /usr/lib/floflis/config
-   source /usr/lib/floflis/config
+   source /usr/lib/floflis/layers/dna/config
    contents="$(jq ".layer = \"$layer\"" /1/Floflis/system/os.json)" && \
    echo "${contents}" > /1/Floflis/system/os.json
    contents="$(jq ".nxtlayer = \"$nxtlayer\"" /1/Floflis/system/os.json)" && \
